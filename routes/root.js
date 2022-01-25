2d03929ed9c65317b78da100601309d3314a3c5a
Sorrel98 <wp05110@naver.com>
2022-01-25 17:07:44 +0900

    /invitationList
    alter to UnauthorizedError

--- routes/root.js
+++ routes/root.js
@@ -295,10 +295,8 @@ router.get('/invitationList', async (request, response, next) => {
     
     const uid = request.session.uid;    
     if(!uid) {
-        // return next(new AuthError(401));
-        response.writeHead(401);
-        response.end();
-        return;
+        const err = new UnauthorizedError(`Session has no uid. request.session: ${util.inspect(request.session, true, 2, true)}`);
+        return next(err);
     }
 
     let conn, res1;
