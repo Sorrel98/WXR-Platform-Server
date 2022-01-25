27abc695cc7e7977099f53818a1b3b92a2f5baf6
Sorrel98 <wp05110@naver.com>
2022-01-25 11:46:01 +0900

    /logout
    alter to UnauthorizedError

--- routes/root.js
+++ routes/root.js
@@ -152,10 +152,8 @@ router.post('/logout', (request, response) => {
     
     const id = request.session.uid;
     if(!id) {
-        // return next(new AuthError(401));
-        response.writeHead(401);
-        response.end();
-        return;
+        const err = new UnauthorizedError(`Session has no uid. request.session: ${util.inspect(request.session, true, 2, true)}`);
+        return next(err);
     }
 
     // await util.promisify(request.session.destroy)();
