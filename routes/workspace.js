24bb8f0c0f83edb18210cb4e9783555ec69f3f73
hyb9579 <hyb9579@gmail.com>
2022-01-26 17:47:00 +0900

    Fix '/leave'

--- routes/workspace.js
+++ routes/workspace.js
@@ -480,18 +480,12 @@ router.post('/leave', async (request, response, next) => {
     
     const uid = request.session.uid;
     if(!uid) {
-        // return next(new AuthError(401));
-        response.writeHead(401);
-        response.end();
-        return;
+        next(new DBError("Session has no uid.", 401));
     }
     
     const wid = request.body.wid;
     if(!wid) {
-        // return next(new AuthError(400));
-        response.writeHead(400);
-        response.end();
-        return;
+        next(new DBError("There is no wid.", 400));
     }
 
     let conn;
